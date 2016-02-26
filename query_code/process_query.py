6ed53a9a90d976ce37b75ca933b880dcba91ec6d
Julia <juliakj@princeton.edu>
2016-02-26 16:13:10 -0500

    combine dicts in process_query

--- query_code/process_query.py
+++ query_code/process_query.py
@@ -10,9 +10,17 @@ def main(argv):
     map = getMap()
 
 def getMap():
-    for filename in os.listdir('./map_pickles'):
-        print filename
-   # pickle.dump(map, open('dat2.p', 'wb'))
+	path = './map_pickles'
+	full = {}
+	for filename in os.listdir(path):
+		temp = pickle.load(open(path + '/' + filename, 'rb'))
+		for specialty in temp:
+			if specialty not in full:
+				full[specialty] = temp[specialty]
+			else:
+				full[specialty] += " "
+				full[specialty] += temp[specialty]
+	print len(full)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
