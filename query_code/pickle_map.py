d064f47b3446c392c84536d0e1ddd0b566946a24
Ubuntu <ubuntu@ip-172-31-35-133.us-west-2.compute.internal>
2016-02-26 05:15:02 +0000

    added query code

---
+++ query_code/pickle_map.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import MySQLdb
+import pickle
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="Galdorhavens0!",  # your password
+                     db="db")        # name of the data base
+
+cur = db.cursor()
+
+cur.execute("SELECT p.specialty, s.descrip FROM providers p, services s where p.npi=s.npi order by p.npi asc limit 1000000, 1000000")
+
+print 'selection done'
+
+map = {}
+for row in cur.fetchall():
+    if row[0].upper() not in map:
+        map[row[0].upper()] = row[1]
+        print row[0]
+    else:
+        map[row[0].upper()] += " "
+        map[row[0].upper()] += row[1]
+print len(map)
+#for specialty in map:
+#    print specialty + " " + map[specialty]
+pickle.dump(map, open('dat2.p', 'wb'))
+
+db.close()
