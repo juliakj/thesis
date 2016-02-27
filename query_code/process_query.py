e9bfd08949f0fad94b41b866b01fee03a14a2a68
Ubuntu <ubuntu@ip-172-31-35-133.us-west-2.compute.internal>
2016-02-27 21:31:38 +0000

    basic tfidf implemented

--- query_code/process_query.py
+++ query_code/process_query.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
+from heapq import nlargest
 import pickle
 import sys
 import glob
 import os
+from nltk import corpus
 import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+import string
+import itertools
+from operator import itemgetter
+from itertools import izip
+from scipy.sparse import coo_matrix
 
 def main(argv):
     query = argv[0]
@@ -16,17 +24,58 @@ def getMap():
 	full = {}
 	for filename in os.listdir(path):
 		temp = pickle.load(open(path + '/' + filename, 'rb'))
+                i = 0
 		for specialty in temp:
-			if specialty not in full:
-				full[specialty] = temp[specialty]
-			else:
-				full[specialty] += " "
-				full[specialty] += temp[specialty]
+                    i += 1
+                    if specialty not in full:
+                        full[specialty] = temp[specialty]
+                    else:
+                        full[specialty] += " "
+                        full[specialty] += temp[specialty]
+                    if i == 15:
+                        break
 	print len(full)
+        return full
 
 def getTopFrequencies(map):
-	for specialty in map:
-		allWords = nltk.tokenize.word_tokenize(map[specialty])
+    #stopwords = corpus.stopwords.words('english')
+
+    tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+    token_dict = {}
+    #for specialty in map:
+     #   map[specialty].lower().translate(None, string.punctuation)
+    print 'start fit'
+
+    tfs = tfidf.fit_transform(map.values())
+    print 'fit done'
+    for specialty in map:
+        response = tfidf.transform([map[specialty]])
+        feature_names = tfidf.get_feature_names()
+        print specialty
+
+        featDict = []
+        for col in response.nonzero()[1]:
+            featDict.append((feature_names[col], response[0,col]))
+
+        print nlargest(20, featDict, key=lambda e:e[1])
+        print ''
+
+def sort_matrix(m):
+    tuples = izip(m.row, m.col, m.data)
+    return sorted(tuples, key=lambda x: (x[2], x[2]))
+
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+#    stems = []
+#    for item in tokens:
+#        stems.append(PorterStemmer().stem(item))
+    return tokens
+
+#        allWords = nltk.tokenize.word_tokenize(map[specialty])
+#        allWordDist = nltk.FreqDist(w.lower() for w in allWords)
+#        allWordExceptStopDist = nltk.FreqDist(w.lower() for w in allWords if w not in stopwords) 
+#        mostCommon= allWordExceptStopDist.most_common(10)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
