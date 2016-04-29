3c4676b38e4eef8aee8dbe3e1d9bc34e0697c51e
Ubuntu <ubuntu@ip-172-31-35-133.us-west-2.compute.internal>
2016-04-29 19:41:41 +0000

    updates to interface and composite scoring

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flaskext.mysql import MySQL
 import sys
 sys.path.insert(0, '/home/ubuntu/mygit/query_code/')
-import get_providers, process_query
+import get_providers, process_query, revised, revised2
 import pickle
 from flask import request
 from flask import render_template
@@ -26,23 +26,65 @@ def home():
 def my_form_post():
     text = request.form['text']
     state = request.form['state']
-    a = get_providers.get([text, state])
-    l = a[5:]
-    return render_template("home.html", results=l)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    a = revised2.get([text, state], cursor)
+
+    return render_template("home.html", results=a)
 
 
+@app.route("/npi/<npi>")
+def provider(npi):
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  cursor.execute("SELECT * from providers where npi=" + str(npi))
+  personal = cursor.fetchall()[0]
+  cursor.execute("SELECT * from locations where npi=" + str(npi))
+
+  location = cursor.fetchall()[0]
 
 
+  cursor.execute("SELECT * from violations where npi=" + str(npi))
 
-#  conn = mysql.connect()
- # cursor = conn.cursor()
- # cursor.execute("SELECT * from violations")
+  violations = cursor.fetchall()
+
+  cursor.execute("SELECT descrip, submitted_charge from services where npi=" + str(npi) + " limit 5")
+  services = cursor.fetchall()
+
+  if personal[3] != "":
+    name = personal[2] + " " + personal[3] + ". " + personal[1]
+  else:
+    name = personal[2] + " " + personal[1]
   
-  #data = cursor.fetchall()[0]
-#  data = process_query.process('pap smear') #get_providers.get(['pap smear', 'KY'])
-  #map = pickle.load(open('fullSpecialtiesMap.p', 'rb'))
+  specialty = personal[6]
+  cred = personal[4]
+  gender = personal[5]
+  if personal[7] == "Y":
+    medicare = "Yes"
+  else:
+    medicare = "No"
 
- # return str(data)
+  if personal[8] == "OTHER" or personal[8] == "":
+    school = "UNKNOWN"
+  else:
+    school = personal[8]
+  
+  if personal[9] == 0 or personal[9] == "":
+    year = "UNKNOWN"
+  else:
+    year = personal[9]
+
+  address = str(location[2]) + ", " + str(location[3]) + ", " + str(location[5]) + "\n" + str(location[4])
+  
+
+
+
+  return render_template("npi.html", name=name, specialty=specialty, gender=gender, cred=cred, medicare=medicare, school=school, year=year, address=address, violations=violations, services=services)
+  #return str(personal)
+
+
+
+
 
 
 if __name__ == '__main__':
--- query_code/cluster.py
+++ query_code/cluster.py
@@ -20,12 +20,51 @@ import networkx as nx
 
 
 def main():
-    npis = getData()
-    print len(npis)
-    print len(set(npis))
+    '''
+    violations = getViolationData()
+    print len(violations)
+    print len(set(violations))
 
+    providers = getallNpis()
+    print len(providers)
 
-def getData():
+    d = dict((el[0],[1 if el[0] in violations else 0, 0]) for el in providers)
+    '''
+    d = pickle.load(open("currentQuality.p", "rb"))
+   
+    services = getServiceData()
+#    del providers
+    print "got services"
+    for s in services:
+        d[s[0]][1] += s[1]
+
+    print d[1003000126]
+
+    pickle.dump(d, open("currentQuality.p", "wb"))
+
+def getServiceData():
+    db = MySQLdb.connect(host="localhost",    # your host, usually localhost                                                                                               
+                         user="root",         # your username                                                                                                               
+                         passwd="Galdorhavens0!",  # your password                                                                                                          
+                         db="db")        # name of the data base                                                                                                            
+                                                                                                                                                                             
+    cur = db.cursor()
+
+    cur.execute("SELECT npi, cnt FROM services limit 8500000, 382030")
+    return cur.fetchall()
+
+def getallNpis():
+     db = MySQLdb.connect(host="localhost",    # your host, usually localhost                                                                                               
+                         user="root",         # your username                                                                                                               
+                         passwd="Galdorhavens0!",  # your password                                                                                                          
+                         db="db")        # name of the data base                                                                                                             
+     cur = db.cursor()
+
+     cur.execute("SELECT npi FROM providers")
+
+     return cur.fetchall()
+
+def getViolationData():
     db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                          user="root",         # your username
                          passwd="Galdorhavens0!",  # your password
